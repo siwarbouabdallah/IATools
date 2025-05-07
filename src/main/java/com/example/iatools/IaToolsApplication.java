b1011f8fc8e2267079ca5ddee8e5c3effe5ab100
siwarbouabdellah <siwar.bouabdellah@esprit.tn>
2025-05-07 18:40:44 +0100

    intial commit

---
+++ src/main/java/com/example/iatools/IaToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.iatools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iatools/IaToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iatools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
